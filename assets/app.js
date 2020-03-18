1153cb2cb856d4e6f8bd865c017e70a3bd022001
laudeon <louis.audeon@mail.be>
2020-03-18 18:00:10 +0100

    initial commit, mvp

---
+++ assets/app.js
@@ -0,0 +1,22 @@
+const App = (function (ctx) {
+  ctx.compteurElem = document.querySelector('#compteur p')
+
+  ctx.start = () => {
+    while (!moment) setTimeout(ctx.start, 500)
+    moment.locale("fr")
+
+    setInterval(() => ctx.updateCompteurElem(ctx.calcDiff()), 1000)
+  }
+
+  ctx.calcDiff = () => {
+    const now = new moment()
+    const startedOn = new moment("20200316", "YYYYMMDD")
+    const diff = moment.duration(now.diff(startedOn))
+
+    return diff
+  }
+
+  ctx.updateCompteurElem = diff => ctx.compteurElem.innerHTML = 'Tu es confiné⋅e depuis ' + diff.days() + ' jours, ' + diff.hours() + ' heures, ' + diff.minutes() + ' minutes et ' + diff.seconds() + ' secondes !'
+
+  return ctx
+})({})
